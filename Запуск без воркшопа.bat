b5b6b97ae8cb78084d181066ac215df420a37ee7
makcimbx <makcimbx@gmail.com>
2017-11-11 12:04:16 +0300

    new

--- Запуск без воркшопа.bat
+++ Запуск без воркшопа.bat
@@ -3,6 +3,6 @@ cls
 title srcds.com Watchdog 
 :srcds 
 echo (%time%) srcds started. 
-start /wait srcds.exe -game garrysmod -console +gamemode darkrp +map rp_ussr_military_tlc -authkey 47B65BAE537D8A03D5617AF2AF4F27A6 +maxplayers 1337 +port 27015 +r_hunkalloclightmaps 0  -debug -tickrate 66 //+host_workshop_collection 938873891
+start /wait srcds.exe -game garrysmod -console +gamemode sandbox +map gm_construct -authkey 47B65BAE537D8A03D5617AF2AF4F27A6 +maxplayers 1337 +port 27015 +r_hunkalloclightmaps 0  -debug -tickrate 16 //+host_workshop_collection 938873891
 goto srcds
 quit
