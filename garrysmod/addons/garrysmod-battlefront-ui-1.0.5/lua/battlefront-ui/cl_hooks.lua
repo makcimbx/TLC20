78137a61a06d8cfb881d0a0dd16b5e2c6863bc42
Ever68 <evergames68@gmail.com>
2017-11-11 16:56:53 +0300

    1

--- garrysmod/addons/garrysmod-battlefront-ui-1.0.5/lua/battlefront-ui/cl_hooks.lua
+++ garrysmod/addons/garrysmod-battlefront-ui-1.0.5/lua/battlefront-ui/cl_hooks.lua
@@ -5,21 +5,3 @@
 
     Developer information: 76561198045250557 : 4601 : 16520
 ]]--
-
-hook.Add( "PlayerButtonUp", "bfUI_keybinds", function( player, buttonId )
-	if not IsFirstTimePredicted() then return end
-	if player ~= LocalPlayer() then return end
-	if gui.IsGameUIVisible() then return end
-	if player:IsTyping() then return end
-
-    local chosenKey = bfUI.getUnEditableData( "menu_key", KEY_ESCAPE )
-    if buttonId ~= chosenKey then return end
-
-    bfUI.toggleMenu()
-end )
-
-concommand.Add( "bfui_toggle", bfUI.toggleMenu )
-
-if bfUI.getClientData( "auto_open_on_join", true ) then
-    hook.Add( "InitPostEntity", "bfUI", bfUI.toggleMenu )
-end
