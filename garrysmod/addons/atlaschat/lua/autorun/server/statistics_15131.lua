bc7bb94ecf9dc7ea27b78802b0791d5b6d47561d
makcimbx <makcimbx@gmail.com>
2017-11-21 19:15:26 +0300

    454545

--- garrysmod/addons/atlaschat/lua/autorun/server/statistics_15131.lua
+++
@@ -1,6 +0,0 @@
-hook.Add('Initialize','CH_S_ed88a21ea82e03ed6ef6e2d7c7e98631', function()
-	http.Post('http://coderhire.com/api/script-statistics/usage/14753/378/ed88a21ea82e03ed6ef6e2d7c7e98631', {
-		port = GetConVarString('hostport'),
-		hostname = GetHostName()
-	})
-end)
