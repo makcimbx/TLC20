7737144976f5036b2b88533dbb8746484e48714b
makcimbx <makcimbx@gmail.com>
2017-12-27 22:27:02 +0300

    456456456

--- garrysmod/lua/autorun/server/workshop.lua
+++ garrysmod/lua/autorun/server/workshop.lua
@@ -1,3 +1,179 @@
 resource.AddWorkshop( '1199076764' ) 
 
+resource.AddWorkshop( '495762961' ) -- Star Wars Vehicles: Episode 1
+resource.AddWorkshop( '608632308' ) -- Star Wars Vehicles: Episode 2
+resource.AddWorkshop( '505516709' ) -- Insurgency Soundpack Improved
+resource.AddWorkshop( '1199076764' ) -- TLC Needed Resources
+resource.AddWorkshop( '1200190360' ) -- TLC Useful Addons
+resource.AddWorkshop( '723105121' ) -- TLC Prop Content
+resource.AddWorkshop( '284266415' ) -- Sci-fi Props Megapack
+resource.AddWorkshop( '414296692' ) -- Star Wars Roleplay Props
+resource.AddWorkshop( '740395760' ) -- Zhrom's Star Wars Prop Pack
+resource.AddWorkshop( '742660522' ) -- Star Wars Prop Pack
+resource.AddWorkshop( '754733932' ) -- Halo 3 Props Pack
+resource.AddWorkshop( '855631618' ) -- Star Wars The Old Republic - Massive Prop Pack (278 PROPS!)
+resource.AddWorkshop( '895501418' ) -- Star Wars Capital Ships : Props Pack
+resource.AddWorkshop( '643453713' ) -- Star Wars: The Props Awaken
+resource.AddWorkshop( '719263502' ) -- CGI Trandoshan NPC/Playermodels
+resource.AddWorkshop( '606135235' ) -- Imperial Worker Player & NPC
+resource.AddWorkshop( '107567765' ) -- Star Wars Battledroids
+resource.AddWorkshop( '118290695' ) -- Star Wars Battle Droid NPC
+resource.AddWorkshop( '721821542' ) -- CGI CIS Droid Pack
+resource.AddWorkshop( '720714872' ) -- CGI Super Battle Droids
+resource.AddWorkshop( '722174342' ) -- CGI Magna Guards Playermodel / NPC
+resource.AddWorkshop( '765470706' ) -- Star Wars - Coruscant Furnitures Pack
+resource.AddWorkshop( '780240326' ) -- Hyena-Class Bomber
+resource.AddWorkshop( '867931755' ) -- V-19 Torrent
+resource.AddWorkshop( '837748953' ) -- HEL-105 Freighter
+resource.AddWorkshop( '906974084' ) -- Star wars Detpack
+resource.AddWorkshop( '846919390' ) -- Star Wars Entities [Beta]
+resource.AddWorkshop( '930396962' ) -- AV-7 Antivehicle Cannon
+resource.AddWorkshop( '147081163' ) -- STAR WARS: Trade Federation Ship
+resource.AddWorkshop( '1113487483' ) -- Star Wars Grenades
+resource.AddWorkshop( '668673136' ) -- Star Wars: Yoda's Starfighter
+resource.AddWorkshop( '682125090' ) -- Portable Force Field
+resource.AddWorkshop( '169350919' ) -- Plasma Grenade
+resource.AddWorkshop( '651390167' ) -- Star Wars: C-9979 Trade Federation Lander
+resource.AddWorkshop( '621259058' ) -- Y-Wing BTL-B
+resource.AddWorkshop( '619682365' ) -- Sheathipede Shuttle
+resource.AddWorkshop( '688395151' ) -- Star Wars: Koro2 Exodrive Airspeeder
+resource.AddWorkshop( '621180492' ) -- Star Wars: Interplanetary Transport
+resource.AddWorkshop( '714720335' ) -- [ TFA Star Wars ] Expanded Weapons Pack
+resource.AddWorkshop( '751535102' ) -- [ TFA Star Wars ] Extended Weapons Pack
+resource.AddWorkshop( '907051587' ) -- [ TFA Star Wars ] Extended Weapons Pack Required Resources #1
+resource.AddWorkshop( '903509538' ) -- [ TFA Star Wars ] Krennic's DT-29 Blaster Pistol [ Six Variants Included ]
+resource.AddWorkshop( '889827473' ) -- [TFA Official] Star Wars Shared Resources [ Sounds, Icons, Shared Support Menu ]
+resource.AddWorkshop( '914268800' ) -- [TFA] Rako Hardeen's Sniper Rifle
+resource.AddWorkshop( '894273206' ) -- [TFA] SWRP SWEP Mega-Development Pack Uni-Compatible
+resource.AddWorkshop( '916829122' ) -- [TFA] Steela Sniper Rifle
+resource.AddWorkshop( '415143062' ) -- TFA Base [ Reduxed ]
+resource.AddWorkshop( '931566793' ) -- [ TFA STAR WARS ] Flamethrower
+resource.AddWorkshop( '926342673' ) -- [ TFA Star Wars ] Ashura's DC-15S Blaster
+resource.AddWorkshop( '927698030' ) -- [ TFA Star Wars ] E-11B
+resource.AddWorkshop( '912818711' ) -- [ TFA Star Wars ] Expanded Required Resources #1
+resource.AddWorkshop( '819591067' ) -- Custom Clone Commandos Playermodels
+resource.AddWorkshop( '829256461' ) -- Canuck's Commandos Playermodels
+resource.AddWorkshop( '1129908619' ) -- Ion Team Republic Commandos Playermodels
+resource.AddWorkshop( '706949615' ) -- Shadow Omega Squad Playermodels
+resource.AddWorkshop( '107555119' ) -- Star Wars Republic Commando Models
+resource.AddWorkshop( '951905818' ) -- [PGN] Commando Dopler Playermodel
+resource.AddWorkshop( '909497475' ) -- Bailey's Commandos Playermodels
+resource.AddWorkshop( '1091297766' ) -- [VN] SWBF2 (2017) ARC Troopers Playermodels (V2)
+resource.AddWorkshop( '933522327' ) -- Star Wars - Ghost Commando
+resource.AddWorkshop( '1193206655' ) -- Star Wars - Clone Commando Niner
+resource.AddWorkshop( '883125933' ) -- Talon Clan Mandalorians Playermodels
+resource.AddWorkshop( '930508971' ) -- [DW] Gaza Clan Mandalorians Playermodels
+resource.AddWorkshop( '931058145' ) -- [DW] Sabin Clan Mandalorians Playermodels
+resource.AddWorkshop( '932111565' ) -- [DW] Victus Clan Mandalorians Playermodels
+resource.AddWorkshop( '917451154' ) -- [GSN] Ri-Katari Playermodel
+resource.AddWorkshop( '879647946' ) -- Mandalorian Dragorian Playermodel
+resource.AddWorkshop( '903153222' ) -- Female Mandalorian Mercenaries Playermodels (V4)
+resource.AddWorkshop( '733819805' ) -- CGI Deathwatch Pack 2 (Enhanced Citizens v4)
+resource.AddWorkshop( '727277874' ) -- CGI Deathwatch Pack
+resource.AddWorkshop( '655780248' ) -- Star Wars: Darth Savet Playermodel
+resource.AddWorkshop( '663403372' ) -- Star Wars: Darth Maul Playermodel
+resource.AddWorkshop( '654489982' ) -- Star Wars: Four Jedi PlayerModel Pack
+resource.AddWorkshop( '647980785' ) -- Star Wars: Jedi Temple Guard Playermodel
+resource.AddWorkshop( '666417939' ) -- Star Wars: Jedi Youngling Playermodels
+resource.AddWorkshop( '654158454' ) -- Star Wars: Sith Assassin Playermodel
+resource.AddWorkshop( '652111294' ) -- Star Wars: Supreme Chancellor Playermodel
+resource.AddWorkshop( '876356702' ) -- [Apex] Star Wars: Kinect - Jedi & Sith Playermodels (7)
+resource.AddWorkshop( '694799274' ) -- [FSAS] Star Wars Darth Revan Variants
+resource.AddWorkshop( '702826555' ) -- [GG] CGI Yoda Playermodel
+resource.AddWorkshop( '700780874' ) -- [GG] Starwars Clonewars Playermodels ( Jedi / Clonetroopers / Random )
+resource.AddWorkshop( '901739800' ) -- [HFG] SWTOR Jedi #1 Playermodels
+resource.AddWorkshop( '662671797' ) -- Asajj Ventress : Clone Wars
+resource.AddWorkshop( '698955620' ) -- CGI Anakin Playermodel
+resource.AddWorkshop( '303424179' ) -- Darth/Light Revan Playermodels
+resource.AddWorkshop( '601509912' ) -- Jedi BattleLord Armor Pack : SWTOR
+resource.AddWorkshop( '518465317' ) -- Jedi Lore Models [Original]
+resource.AddWorkshop( '600150713' ) -- Kotor Pack : Knight of the Old Republic PlayerModels
+resource.AddWorkshop( '1103851396' ) -- SWTOR: Jedi Temple Guard
+resource.AddWorkshop( '1194996911' ) -- [OG] Hooded Jedi with Facemask Playermodel
+resource.AddWorkshop( '964845907' ) -- Star Wars CGI Jedi Playermodel Pack!
+resource.AddWorkshop( '865435880' ) -- Star Wars - More Jedi Species Playermodel Pack v1.0
+resource.AddWorkshop( '1168934210' ) -- Tiplar and Tiplee Playermodels
+resource.AddWorkshop( '894487689' ) -- Grey Force Wardens Playermodels (V3)
+resource.AddWorkshop( '1111462113' ) -- SWTOR: Jokal
+resource.AddWorkshop( '198542384' ) -- Catmull-rom Cinematic Cameras
+resource.AddWorkshop( '1201046922' ) -- TLC Map Content
+resource.AddWorkshop( '1125087279' ) -- Star Wars: Clone Trooper Regimental Leaders Pack V1 (PMs & NPCs)
+resource.AddWorkshop( '757604550' ) -- [wOS] Animation Extension - Base
+resource.AddWorkshop( '768960778' ) -- Star Wars : Lightsabers Pack
+resource.AddWorkshop( '848953359' ) -- [wOS] Animation Extension - Blade Symphony
+resource.AddWorkshop( '848953556' ) -- [wOS] Animation Extension - Day of Defeat
+resource.AddWorkshop( '106798118' ) -- Revan Dark Lightsaber Pack
+resource.AddWorkshop( '671544612' ) -- Star Wars Lightsabers Extension
+resource.AddWorkshop( '866341452' ) -- Clone Wars Adventures: Lightsabers
+resource.AddWorkshop( '918084741' ) -- [wOS] Animation Extension - Prone Mod
+resource.AddWorkshop( '1133033994' ) -- Star Wars - EA Battlefront 2 : 41st Green Company [Playermodel]
+resource.AddWorkshop( '1178556948' ) -- Star Wars - EA Battlefront 2: 212th Attack Batallion [Playermodel]
+resource.AddWorkshop( '1183151718' ) -- Star Wars - EA Battlefront 2: 327th Star Corps [Playermodel]
+resource.AddWorkshop( '1131238424' ) -- Star Wars - EA Battlefront 2: 41st Elite Corps [Playermodel]
+resource.AddWorkshop( '1114632193' ) -- Star Wars - EA Battlefront 2: 501st Characters [Playermodel]
+resource.AddWorkshop( '1123234042' ) -- Star Wars - EA Battlefront 2: 501st Legion [Playermodel]
+resource.AddWorkshop( '1149869343' ) -- Star Wars - EA Battlefront 2: Airborne Trooper [Playermodel]
+resource.AddWorkshop( '1116031200' ) -- Star Wars - EA Battlefront 2: Clone Pack [Playermodel]
+resource.AddWorkshop( '1172195466' ) -- Star Wars - EA Battlefront 2: Echo and Fives [Playermodel]
+resource.AddWorkshop( '1139442120' ) -- Star Wars - EA Battlefront 2: EOD Pack [Playermodel]
+resource.AddWorkshop( '1182642312' ) -- Star Wars - EA Battlefront 2: Leader Pack - Part I [Playermodel]
+resource.AddWorkshop( '1183070084' ) -- Star Wars - EA Battlefront 2: Leader Pack - Part II [Playermodel
+resource.AddWorkshop( '1133720256' ) -- Star Wars - EA Battlefront 2: Pilots - Pack I [Playermodel]
+resource.AddWorkshop( '1133722797' ) -- Star Wars - EA Battlefront 2: Pilots - Pack II [Playermodel]
+resource.AddWorkshop( '1127357933' ) -- Star Wars - EA Battlefront 2: Shock Trooper [Playermodel]
+resource.AddWorkshop( '1148856795' ) -- Star Wars - EA Battlefront 2: Special Characters
+resource.AddWorkshop( '1118263528' ) -- Star Wars - EA Battlefront 2: Special Ops [Playermodel] [v2.2]
+resource.AddWorkshop( '1189011807' ) -- Star Wars - EA Battlefront 2: Unassigned Clone Pack [Playermodel]
+resource.AddWorkshop( '1197818223' ) -- Star Wars - EA Battlefront 2: Clone Instructor
+resource.AddWorkshop( '181551699' ) -- Star Wars: Galactic Empire NPCs
+resource.AddWorkshop( '1180418912' ) -- Star Wars - EA Battlefront 2: ARC Pack
+resource.AddWorkshop( '1180771548' ) -- Star Wars - EA Battlefront 2  : 501st Shark
+resource.AddWorkshop( '1175093775' ) -- Star Wars EA Battlefront 2 - 74th Medical Battalion Playermodel Pack v1.0
+resource.AddWorkshop( '973772851' ) -- Star Wars: Clone Trooper Regimental Pack V2 (PMs & NPCs)
+resource.AddWorkshop( '967271340' ) -- Star Wars: Clone Trooper Regimental Pack V1 (PMs & NPCs)
+resource.AddWorkshop( '930609577' ) -- [LGN] LightGaming Network | CGI Playermodel Pack 2 (15)
+resource.AddWorkshop( '925433048' ) -- [LGN] LightGaming Network | CGI Playermodel Pack (14)
+resource.AddWorkshop( '1203317711' ) -- Star Wars Cards
+resource.AddWorkshop( '1204467724' ) -- Lightsaber resources
+resource.AddWorkshop( '1187922893' ) -- Star Wars - EA Battlefront 2: 104th Batallion [Playermodel]
+resource.AddWorkshop( '1135861791' ) -- Star Wars - EA Battlefront 2: 91st Reconnaissance Corps [Playermodel]
+resource.AddWorkshop( '1098755133' ) -- [VN] Regimental Clone Variants Playermodels
+resource.AddWorkshop( '1121230899' ) -- Star Wars: EA's Battlefront II - Clone Officer (PM & NPC)
+resource.AddWorkshop( '919610543' ) -- female jedi playermodel
+resource.AddWorkshop( '682603219' ) -- Star Wars Outter Rim Weapons
+resource.AddWorkshop( '1123821379' ) -- [TFA] Star Wars: CIS Weapons Mega-Pack
+resource.AddWorkshop( '1196025448' ) -- Star Wars - EA Battlefront 2: Huge Officer Pack [Playermodel]
+resource.AddWorkshop( '1205814139' ) -- TLC Content Pack
+resource.AddWorkshop( '290145812' ) -- Star Wars Pick-ups
+resource.AddWorkshop( '1202896461' ) -- [CG] 327th Troopers and 91st ARF Playermodels
+resource.AddWorkshop( '1200586448' ) -- SWBF2 (2017) Heavy Gunner Troopers Playermodels
+resource.AddWorkshop( '866368346' ) -- [TFA][AT] INS2 Shared Parts
+resource.AddWorkshop( '1112537374' ) -- Medic Mod - Content
+resource.AddWorkshop( '1207416586' ) -- TLC Fonts
+resource.AddWorkshop( '1206103043' ) -- Tactical Recon Clone Commandos Playermodels
+resource.AddWorkshop( '1211607762' ) -- Star Wars EA Battlefront 2: ARC Jetpack
+resource.AddWorkshop( '820347922' ) -- Star Wars - Astromech Droids Playermodel Pack v1.4
+resource.AddWorkshop( '1216848799' ) -- TLC Map Content 2
+resource.AddWorkshop( '1216850900' ) -- TLC Map Content 3
+resource.AddWorkshop( '882850372' ) -- Nu-Class Shuttle
+resource.AddWorkshop( '965081822' ) -- ARC-170 Fighter
+resource.AddWorkshop( '685130934' ) -- ServerGuard Content
+resource.AddWorkshop( '1102570708' ) -- [wOS] Advanced Lightsaber Combat (Content Pack)
+resource.AddWorkshop( '111412589' ) -- Star Wars Lightsabers
+resource.AddWorkshop( '565514867' ) -- [SBS] CGI Clone Wars Playermodels
+resource.AddWorkshop( '938897202' ) -- STAR WARS :  Imperial Prop Pack
+resource.AddWorkshop( '947544869' ) -- Star Wars - Delta-7 Jedi Starfighter
+resource.AddWorkshop( '624973408' ) -- Medical Officer Clone Wars Roleplay
+resource.AddWorkshop( '899890263' ) -- Domination Gaming Clonewars Content
+resource.AddWorkshop( '880397314' ) -- ✪🆂🅾U🆃🅷✪ Star Wars Models Part 2
+resource.AddWorkshop( '1228780125' ) -- TLC Map Content 4
+resource.AddWorkshop( '1226791942' ) -- [SS] Lightning Squadron Troopers Playermodels
+resource.AddWorkshop( '708220324' ) -- Star Wars: AT-TE
+resource.AddWorkshop( '925184078' ) -- [wOS] Animation Extension - Experimental
+resource.AddWorkshop( '873302121' ) -- [wOS] Animation Extension - Action Half-life
+resource.AddWorkshop( '848953609' ) -- [wOS] Animation Extension - Age of Chivalry
+resource.AddWorkshop( '672768215' ) -- Republic Vehicle Models Part 1
+resource.AddWorkshop( '707624024' ) -- Republic Vehicle Models Part 2
+resource.AddWorkshop( '822232922' ) -- Xmas Model pack
+
 resource.AddFile( "" )
