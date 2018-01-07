8c1d54c66e1fdffc5c431aeedfcfb9b0eb4f8707
Ever68 <evergames68@gmail.com>
2018-01-07 12:09:58 +0300

    324

--- garrysmod/addons/registrationtest/lua/rt/sh_rtconfig.lua
+++ garrysmod/addons/registrationtest/lua/rt/sh_rtconfig.lua
@@ -20,7 +20,7 @@ hook.Add("loadCustomDarkRPItems","Ever_loadCustomDarkRPItems",function()
 		[2] = {name = "212",prof = "212-й Клон Солдат",cmd = "212ct",category = "212 AB"},
 		[3] = {name = "41",prof = "41-й Клон Солдат",cmd = "41ct",category = "41st legion"},
 		[4] = {name = "91",prof = "91-й Клон Солдат",cmd = "91ct",category = "91st Recon Corps"},
-		[5] = {name = "74",prof = "74-й Клон Солдат",cmd = "74ct",category = "74th medicacal corps"},
+		[5] = {name = "74",prof = "74-й Клон Солдат",cmd = "74ct",category = "74th medical corps"},
 		[6] = {name = "EOD",prof = "EOD Клон Солдат",cmd = "eodct",category = "EOD"},
 		[7] = {name = "327",prof = "327-й Клон Солдат",cmd = "327ct",category = "327st"},
 		[8] = {name = "104",prof = "104-й Клон Солдат",cmd = "104ct",category = "104st"},
